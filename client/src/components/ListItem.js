10ad91a99440409867d883d6793dd1e05a9b424d
diegogglez <diegomiller7@gmail.com>
2023-02-27 16:28:44 +0100

    Feat: adding more styles and structure to the List Item component

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import ProgressBar from "./ProgressBar";
+import TickIcon from "./TickIcon";
 
-const ListItem = ({task}) => {
+const ListItem = ({ task }) => {
   return (
-    <div className='list-item'>
-      <p>{task.title}</p>
-    </div>
-  )
-}
+    <li className="list-item">
+      <div className="info-container">
+        <TickIcon/>
+        <p className="task-title">{task.title}</p>
+        <ProgressBar />
+      </div>
+      <div className="button-container">
+        <button className="edit">EDIT</button>
+        <button className="delete">DELETE</button>
+      </div>
+    </li>
+  );
+};
 
-export default ListItem
+export default ListItem;
